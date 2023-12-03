5d21adb1db3546dad8fe15a6218e4a881c359062
Rai Pramana <rai.pramana46@gmail.com>
2023-12-03 17:03:55 +0800

    renamed:    css/styleformloginsignup.css -> css/styleformprofile.css 	modified:   editprofile.html 	new file:   img/user2.png 	new file:   js/editprofile.js 	modified:   js/validationtool.js 	modified:   login.html 	modified:   signup.html

--- js/validationtool.js
+++ js/validationtool.js
@@ -15,3 +15,21 @@ function setSuccess(input) {
     // add success class
     inputBox.className = "input-box success";
 }
+
+function setErrorPhoto(input, message) {
+    const inputBox = input.parentElement;
+    const small = inputBox.querySelector("small");
+
+    // add error message inside small
+    small.innerText = message;
+
+    // add error class
+    inputBox.className = "input-box-photo error";
+}
+
+function setSuccessPhoto(input) {
+    const inputBox = input.parentElement; // .input-box
+
+    // add success class
+    inputBox.className = "input-box-photo success";
+}
