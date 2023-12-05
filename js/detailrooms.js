80f6bb9318bab99a34f4f724a6853cc66a9cc9af
Rai Pramana <rai.pramana46@gmail.com>
2023-12-05 13:55:10 +0800

    modified:   css/styledetailrooms.css 	modified:   detailrooms.html 	modified:   js/detailrooms.js

--- js/detailrooms.js
+++ js/detailrooms.js
@@ -1,19 +1,55 @@
-const imgs = document.querySelectorAll('.img-select a');
+const imgs = document.querySelectorAll(".img-select a");
 const imgBtns = [...imgs];
 let imgId = 1;
 
 imgBtns.forEach((imgItem) => {
-    imgItem.addEventListener('click', (event) => {
+    imgItem.addEventListener("click", (event) => {
         event.preventDefault();
         imgId = imgItem.dataset.id;
         slideImage();
     });
 });
 
-function slideImage(){
-    const displayWidth = document.querySelector('.img-showcase img:first-child').clientWidth;
+function slideImage() {
+    const displayWidth = document.querySelector(".img-showcase img:first-child").clientWidth;
 
-    document.querySelector('.img-showcase').style.transform = `translateX(${- (imgId - 1) * displayWidth}px)`;
+    document.querySelector(".img-showcase").style.transform = `translateX(${-(imgId - 1) * displayWidth}px)`;
 }
 
-window.addEventListener('resize', slideImage);
+window.addEventListener("resize", slideImage);
+
+// setting stars rating
+function setRating(rating, totalBooking) {
+    if (!rating || !totalBooking) {
+        document.getElementById("productRating").style.display = "none";
+        return;
+    }
+
+    const stars = document.querySelectorAll("#productRating i");
+    const ratingText = document.querySelector("#productRating span");
+
+    // Reset all stars
+    stars.forEach((star) => {
+        star.classList.remove("fa-star");
+        star.classList.remove("fa-star-half-alt");
+        star.classList.add("fa-star-o");
+    });
+
+    // Set full stars
+    for (let i = 0; i < Math.floor(rating); i++) {
+        stars[i].classList.remove("fa-star-o");
+        stars[i].classList.add("fa-star");
+    }
+
+    // Set half star
+    if (rating % 1 !== 0) {
+        stars[Math.floor(rating)].classList.remove("fa-star-o");
+        stars[Math.floor(rating)].classList.add("fa-star-half-alt");
+    }
+
+    // Set rating text
+    ratingText.textContent = `${rating} (${totalBooking})`;
+}
+
+// Call the function with the desired rating
+setRating(2, 8);
