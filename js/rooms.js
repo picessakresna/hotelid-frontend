76124bc3757d5dcbb78c302ee3a66ed2e0c52516
Rai Pramana <rai.pramana46@gmail.com>
2023-12-06 23:40:19 +0800

    modified:   js/rooms.js

--- js/rooms.js
+++ js/rooms.js
@@ -1,14 +1,20 @@
 let loadMoreBtn = document.querySelector("#load-more");
+let boxes = [...document.querySelectorAll(".container .box-container .box")];
+
 let currentItem = 3;
+loadMoreBtn.style.display = "none";
+
+if (currentItem >= boxes.length) {
+    loadMoreBtn.style.display = "none";
+} else {
+    loadMoreBtn.style.display = "";
+}
 
 loadMoreBtn.onclick = () => {
-    let boxes = [...document.querySelectorAll(".container .box-container .box")];
     for (var i = currentItem; i < currentItem + 3; i++) {
-        boxes[i].style.display = "inline-block";
+        if (i < boxes.length) {
+            boxes[i].style.display = "inline-block";
+        }
     }
     currentItem += 3;
-
-    if (currentItem >= boxes.length) {
-        loadMoreBtn.style.display = "none";
-    }
 };
