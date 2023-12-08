495976affdb1ca2531cf9a7331359019e4fe9703
Rai Pramana <rai.pramana46@gmail.com>
2023-12-08 14:48:52 +0800

    modified:   about.html 	modified:   bookings.html 	modified:   css/styledetailbooks.css 	modified:   css/styledetailrooms.css 	modified:   css/styleexplore.css 	modified:   css/styleformprofile.css 	modified:   css/styleprofile.css 	modified:   css/stylereviews.css 	modified:   detailbookings.html 	modified:   detailrooms.html 	modified:   editprofile.html 	modified:   explore.html 	modified:   formbookings.html 	modified:   home.html 	modified:   index.html 	modified:   js/detailrooms.js 	new file:   js/reviews.js 	modified:   profile.html 	modified:   reviews.html 	modified:   rooms.html

--- js/detailrooms.js
+++ js/detailrooms.js
@@ -54,6 +54,38 @@ function setRating(rating, totalBooking) {
 // Call the function with the desired rating
 setRating(5, 1000);
 
+// setting stars rating reviews
+function setRatingReviews(rating) {
+    if (!rating || rating < 1 || rating > 5) {
+        document.getElementById("reviewsRating").style.display = "none";
+        return;
+    }
+
+    const stars = document.querySelectorAll("#reviewsRating i");
+
+    // Reset all stars
+    stars.forEach((star) => {
+        star.classList.remove("fa-star");
+        star.classList.remove("fa-star-half-alt");
+        star.classList.add("fa-star-o");
+    });
+
+    // Set full stars
+    for (let i = 0; i < Math.floor(rating); i++) {
+        stars[i].classList.remove("fa-star-o");
+        stars[i].classList.add("fa-star");
+    }
+
+    // Set half star
+    if (rating % 1 !== 0) {
+        stars[Math.floor(rating)].classList.remove("fa-star-o");
+        stars[Math.floor(rating)].classList.add("fa-star-half-alt");
+    }
+}
+
+// Call the function with the desired rating
+setRatingReviews(4);
+
 // booking button
 document.getElementById("booking").addEventListener("click", () => {
     window.location.href = "formbookings.html?roomId=1";
